d3e3735678f8f204fe66a0f507ec00fd1ff1ab7f
Krzysztof Menżyk <krzysztof@menzyk.net>
2018-11-30 00:16:47 +0100

    Improve wording, extract functions

--- src/lis.js
+++ src/lis.js
@@ -1,38 +1,64 @@
+const binarySearch = (leftIndex, rightIndex, isOnTheRight) => {
+  if (leftIndex > rightIndex) {
+    return leftIndex;
+  }
+
+  const middle = Math.ceil((leftIndex + rightIndex) / 2);
+
+  return isOnTheRight(middle)
+    ? binarySearch(middle + 1, rightIndex, isOnTheRight)
+    : binarySearch(leftIndex, middle - 1, isOnTheRight);
+};
+
+const findLongestPossibleSubsequenceLengthFor = (element, intermediateSubsequences, inputSequence) =>
+  binarySearch(
+    1,
+    intermediateSubsequences.length - 1,
+    index => inputSequence[intermediateSubsequences[index]] < element
+  );
+
+const reconstructLongestIncreasingSubsequence = (
+  lastElementIndexBySubsequenceLength,
+  predecessorsByIndex,
+  inputSequence
+) => {
+  const longestIncreasingSubsequenceLength = lastElementIndexBySubsequenceLength.length - 1;
+  const longestIncreasingSubsequence = new Array(longestIncreasingSubsequenceLength);
+
+  let lastElementIndex = lastElementIndexBySubsequenceLength[longestIncreasingSubsequenceLength];
+
+  for (index = longestIncreasingSubsequenceLength - 1; index > -1; index--) {
+    longestIncreasingSubsequence[index] = inputSequence[lastElementIndex];
+    lastElementIndex = predecessorsByIndex[lastElementIndex];
+  }
+
+  return longestIncreasingSubsequence;
+};
+
 const longestIncreasingSubsequence = inputSequence => {
   if (!Array.isArray(inputSequence)) {
     throw TypeError('Expected array as an argument.');
   }
 
-  const P = new Array(1);
-  const M = new Array(1);
+  const lastElementIndexBySubsequenceLength = new Array(1);
+  const predecessorsByIndex = new Array(1);
 
-  for (i = 0; i < inputSequence.length; i++) {
-    let lo = 1;
-    let hi = M.length - 1;
+  inputSequence.forEach((element, index) => {
+    const longestSubsequenceLength = findLongestPossibleSubsequenceLengthFor(
+      element,
+      lastElementIndexBySubsequenceLength,
+      inputSequence
+    );
 
-    while (lo <= hi) {
-      let mid = Math.ceil((lo + hi) / 2);
-      if (inputSequence[M[mid]] < inputSequence[i]) {
-        lo = mid + 1;
-      } else {
-        hi = mid - 1;
-      }
-    }
+    predecessorsByIndex[index] = lastElementIndexBySubsequenceLength[longestSubsequenceLength - 1];
+    lastElementIndexBySubsequenceLength[longestSubsequenceLength] = index;
+  });
 
-    const newL = lo;
-    P[i] = M[newL - 1];
-    M[newL] = i;
-  }
-
-  const lis = new Array(M.length - 1);
-  let k = M[M.length - 1];
-
-  for (i = M.length - 2; i > -1; i--) {
-    lis[i] = inputSequence[k];
-    k = P[k];
-  }
-
-  return lis;
+  return reconstructLongestIncreasingSubsequence(
+    lastElementIndexBySubsequenceLength,
+    predecessorsByIndex,
+    inputSequence
+  );
 };
 
 module.exports = longestIncreasingSubsequence;
