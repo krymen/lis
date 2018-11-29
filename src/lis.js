345a98cebf4781c11166bd78dfa2c895307adf64
Krzysztof Menżyk <krzysztof@menzyk.net>
2018-11-29 21:45:20 +0100

    Handle non-array argument

--- src/lis.js
+++ src/lis.js
@@ -1,1 +1,9 @@
-module.exports = () => [];
+const longestIncreasingSubsequence = inputArray => {
+  if (!Array.isArray(inputArray)) {
+    throw TypeError('Expected array as an argument.');
+  }
+
+  return [];
+};
+
+module.exports = longestIncreasingSubsequence;
--- test/lis.test.js
+++ test/lis.test.js
@@ -3,3 +3,7 @@ const lis = require('../src/lis.js');
 it('returns empty array if empty array given', () => {
   expect(lis([])).toEqual([]);
 });
+
+it('throws type error if non-array given', () => {
+  expect(_ => lis(1)).toThrow(TypeError);
+});
