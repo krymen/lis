e7c09b00971cefe6b17e2094d2ba8acaaedbf3e9
Krzysztof Menżyk <krzysztof@menzyk.net>
2018-11-29 23:06:03 +0100

    First full implementation of the algorithm

--- src/lis.js
+++ src/lis.js
@@ -1,9 +1,38 @@
-const longestIncreasingSubsequence = inputArray => {
-  if (!Array.isArray(inputArray)) {
+const longestIncreasingSubsequence = inputSequence => {
+  if (!Array.isArray(inputSequence)) {
     throw TypeError('Expected array as an argument.');
   }
 
-  return [];
+  const P = new Array(1);
+  const M = new Array(1);
+
+  for (i = 0; i < inputSequence.length; i++) {
+    let lo = 1;
+    let hi = M.length - 1;
+
+    while (lo <= hi) {
+      let mid = Math.ceil((lo + hi) / 2);
+      if (inputSequence[M[mid]] < inputSequence[i]) {
+        lo = mid + 1;
+      } else {
+        hi = mid - 1;
+      }
+    }
+
+    const newL = lo;
+    P[i] = M[newL - 1];
+    M[newL] = i;
+  }
+
+  const lis = new Array(M.length - 1);
+  let k = M[M.length - 1];
+
+  for (i = M.length - 2; i > -1; i--) {
+    lis[i] = inputSequence[k];
+    k = P[k];
+  }
+
+  return lis;
 };
 
 module.exports = longestIncreasingSubsequence;
--- test/lis.test.js
+++ test/lis.test.js
@@ -1,9 +1,13 @@
 const lis = require('../src/lis.js');
 
-it('returns empty array if empty array given', () => {
+it('returns empty subsequence if empty sequence given', () => {
   expect(lis([])).toEqual([]);
 });
 
 it('throws type error if non-array given', () => {
   expect(_ => lis(1)).toThrow(TypeError);
+});
+
+it('finds longest increasing subsequence', () => {
+  expect(lis([1, 9, 5, 13, 3, 11, 7, 15, 2, 10, 6, 14, 4, 12, 8, 16])).toEqual([1, 3, 7, 10, 12, 16]);
 });
