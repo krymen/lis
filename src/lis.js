d32d8bf17687088e2ff7806f2a301d245679a4f1
Krzysztof Menżyk <krzysztof@menzyk.net>
2018-11-30 01:14:27 +0100

    Extract intermediate computations initialization

--- src/lis.js
+++ src/lis.js
@@ -1,20 +1,44 @@
-const binarySearch = (leftIndex, rightIndex, isOnTheRight) => {
-  if (leftIndex > rightIndex) {
-    return leftIndex;
+const initializeIntermediateComputations = () => ({
+  lastIndexBySubsequenceLength: new Map(),
+  predecessorsByIndex: new Map(),
+  longestSubsequenceLength: 0,
+
+  updateSubsequenceLengthWithIndex: function(length, index) {
+    this.predecessorsByIndex.set(index, this.lastIndexBySubsequenceLength.get(length - 1));
+    this.lastIndexBySubsequenceLength.set(length, index);
+    this.longestSubsequenceLength = Math.max(length, this.longestSubsequenceLength);
+  },
+
+  lastIndexOfLongestSubsequence: function() {
+    return this.lastIndexBySubsequenceLength.get(this.longestSubsequenceLength);
+  },
+
+  getPredecessor: function(index) {
+    return this.predecessorsByIndex.get(index);
+  },
+
+  getLastIndex: function(length) {
+    return this.lastIndexBySubsequenceLength.get(length);
+  }
+});
+
+const binarySearch = (left, right, isOnTheRight) => {
+  if (left > right) {
+    return left;
   }
 
-  const middle = Math.ceil((leftIndex + rightIndex) / 2);
+  const middle = Math.ceil((left + right) / 2);
 
   return isOnTheRight(middle)
-    ? binarySearch(middle + 1, rightIndex, isOnTheRight)
-    : binarySearch(leftIndex, middle - 1, isOnTheRight);
+    ? binarySearch(middle + 1, right, isOnTheRight)
+    : binarySearch(left, middle - 1, isOnTheRight);
 };
 
 const findLongestPossibleSubsequenceLengthFor = (element, intermediateComputations, inputSequence) =>
   binarySearch(
     1,
     intermediateComputations.longestSubsequenceLength,
-    index => inputSequence[intermediateComputations.lastIndexBySubsequenceLength.get(index)] < element
+    length => inputSequence[intermediateComputations.getLastIndex(length)] < element
   );
 
 const reconstructLongestSubsequence = (intermediateComputations, inputSequence) => {
@@ -35,25 +59,7 @@ const longestIncreasingSubsequence = inputSequence => {
     throw TypeError('Expected array as an argument.');
   }
 
-  const intermediateComputations = {
-    lastIndexBySubsequenceLength: new Map(),
-    predecessorsByIndex: new Map(),
-    longestSubsequenceLength: 0,
-
-    updateSubsequenceLengthWithIndex: function(length, index) {
-      this.predecessorsByIndex.set(index, this.lastIndexBySubsequenceLength.get(length - 1));
-      this.lastIndexBySubsequenceLength.set(length, index);
-      this.longestSubsequenceLength = Math.max(length, this.longestSubsequenceLength);
-    },
-
-    lastIndexOfLongestSubsequence: function() {
-      return this.lastIndexBySubsequenceLength.get(this.longestSubsequenceLength);
-    },
-
-    getPredecessor: function(index) {
-      return this.predecessorsByIndex.get(index);
-    }
-  };
+  const intermediateComputations = initializeIntermediateComputations();
 
   inputSequence.forEach((element, index) => {
     if (typeof element !== 'number') {
